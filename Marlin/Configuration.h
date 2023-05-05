1a637da659c467426f65fd5389f80866f3afb01b
Jordi Sanchez <jordi.programacio@gmail.com>
2023-05-05 18:31:15 +0200

    Add favicon.png file Add:
    default_envs = LPC1768
    '#define MOTHERBOARD BOARD_BTT_SKR_V1_4'

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -141,7 +141,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_MKS_GEN_L
+  #define MOTHERBOARD BOARD_BTT_SKR_V1_4
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
